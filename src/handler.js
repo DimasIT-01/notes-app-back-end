afb639a16d5c389311bc987206c6e7b0e4e8571e
Dimas <dimasb597@gmail.com>
2022-09-02 10:43:39 +0700

    initial commit

---
+++ src/handler.js
@@ -0,0 +1,112 @@
+const {nanoid}=require('nanoid');
+const notes = require('./notes');
+
+const addNoteHandler=(request, h)=>{
+  const {title, tags, body}=request.payload;
+
+  const id=nanoid(16);
+  const createdAt=new Date().toISOString();
+  const updatedAt=createdAt;
+  const newnote={title, tags, body, id, createdAt, updatedAt};
+  notes.push(newnote);
+
+  const isSuccess=notes.filter((note)=>note.id===id).length>0;
+
+  if (isSuccess) {
+    const response=h.response({
+      status: 'success',
+      message: 'Berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response =h.response({
+    status: 'fail',
+    message: 'Gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+const getAllNotesHandler=()=>({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler=(request, h)=>{
+  const {id}=request.params;
+  const note=notes.filter((n)=>n.id===id)[0];
+  if (note!==undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response=h.response({
+    status: 'fail',
+    message: 'catatan tidak ada',
+  });
+  response.code(404);
+  return response;
+};
+
+const editNoteByIdHandler=(request, h)=>{
+  const {id}=request.params;
+  const {tittle, tags, body}=request.payload;
+  const updatedAt=new Date().toISOString();
+
+  const index=notes.findIndex((note)=>note.id===id);
+
+  if (index!==-1) {
+    notes[index]={
+      ...notes[index],
+      tittle,
+      tags,
+      body,
+      updatedAt,
+    };
+    const response=h.response({
+      status: 'success',
+      message: 'Berhasil diupdate',
+    });
+    response.code(200);
+    return response;
+  }
+  const response=h.response({
+    status: 'fail',
+    message: 'gagal update',
+  });
+  response.code(404);
+  return response;
+};
+
+const deleteNotebyIdHandler=(request, h)=>{
+  const {id}=request.params;
+  const index=notes.findIndex((note)=>note.id===id);
+
+  if (index!==-1) {
+    notes.splice(index, 1);
+    const response=h.response({
+      status: 'success',
+      message: 'Berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+  const response=h.response({
+    status: 'fail',
+    message: 'gagal dihapus',
+  });
+  response.code(404);
+  return response;
+};
+module.exports={addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNotebyIdHandler};
